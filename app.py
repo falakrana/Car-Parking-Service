b2715a610a40b452beecd489bdae266733e5a83d
falakrana <ranafalak18@gmail.com>
2025-03-02 11:43:58 +0530

    forth commit...

--- app.py
+++ app.py
@@ -295,6 +295,5 @@ if __name__ == '__main__':
     app.run(debug=True)
 
     import os
-    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment variable
-    app.run(host="0.0.0.0", port=port, debug=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render environment
+    app.run(host="0.0.0.0", port=port)
